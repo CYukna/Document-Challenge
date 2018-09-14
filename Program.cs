0906e7b9f6ebdcfa979f73da5b474d136747bcf8
Connor Yukna <connoryukna@mwc-028143.dhcp.missouri.edu>
2018-09-14 14:30:37 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Document\r");
+
+                string title = "";
+                title = Console.ReadLine();
+
+                Console.WriteLine("Enter Document title:");
+
+                string content = "";
+                content = Console.ReadLine();
+
+                Console.WriteLine("Enter document content: ");
+
+                string fileName = title + ".txt";
+
+                string path = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory.fileName);
+                
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("Exception");
+                }
+
+                Console.WriteLine(fileName + "was successfully saved. The document contains" + content.Length + "characters.");
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+            
+    }
+}
